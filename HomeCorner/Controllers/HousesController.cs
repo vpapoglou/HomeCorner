08aec16fd7d032865d41f2c25534b43f67cb7cb6
Billpapos <vpapoglou@gmail.com>
2019-04-18 14:53:09 +0300

    Edit House, Delete House

--- HomeCorner/Controllers/HousesController.cs
+++ HomeCorner/Controllers/HousesController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -24,12 +25,13 @@ namespace HomeCorner.Controllers
 
         public ActionResult Create()
         {
+            ViewBag.Id = new SelectList(db.Houses, "Id", "Name", "Region", "Address", "ReleaseDate", "Price");
             return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Name")] House house)
+        public ActionResult Create([Bind(Include = "Id,Name,Region,Address,ReleaseDate,Price")] House house)
         {
             if (ModelState.IsValid)
             {
@@ -38,7 +40,75 @@ namespace HomeCorner.Controllers
                 return RedirectToAction("Index");
             }
 
-            return View(house);
+            return View();
+        }
+
+        // GET: Houses/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            House house = db.Houses.Find(id);
+            if (house == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.HouseId = new SelectList(db.Houses, "Id", "Name", house.Id);
+            return View();
+        }
+
+        // POST: Houses/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Region,Address")] House house)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(house).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Id = new SelectList(db.Houses, "Id", "Name", house.Id);
+            return View();
+        }
+
+        // GET: Movies/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            House movie = db.Houses.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View();
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            House house = db.Houses.Find(id);
+            db.Houses.Remove(house);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
--- HomeCorner/Services/HouseDb.cs
+++ HomeCorner/Services/HouseDb.cs
@@ -8,38 +8,50 @@ namespace HomeCorner.Services
 {
     public class HouseDb
     {
-        private static IList<House> _db = new List<House>()
+        public static IEnumerable<House> GetAll()
         {
-            new House() { Id = 1, Name = "White House" },
-            new House() { Id = 2, Name = "Black House" },
-            new House() { Id = 3, Name = "Island House" },
-            new House() { Id = 4, Name = "Mountain House" },
-
-        };
-        public static IList<House> GetAll()
-        {
-            return _db;
+            using (var context = new HomeCornerContext())
+            {
+                return context.Houses.Include("House").ToList();
+            }
         }
-        public static void Add(House customer)
-        {
-            var rnd = new Random();
-            customer.Id = rnd.Next(1, 1000);
-            _db.Add(customer);
-        }
-
-        public static void Update(House movie)
-        {
-
-        }
-
-        public static void Delete(House movie)
-        {
-            _db.Remove(movie);
-        }
-
         public static House GetById(int Id)
         {
-            return _db.FirstOrDefault(c => c.Id == Id);
+            using (var context = new HomeCornerContext())
+            {
+                return context.Houses.Include("House").SingleOrDefault(m => m.Id == Id);
+            }
+        }
+
+        public static void Add(House house)
+        {
+            using (var context = new HomeCornerContext())
+            {
+                context.Houses.Add(house);
+                context.SaveChanges();
+            }
+        }
+
+        public static void Update(House house)
+        {
+            using (var context = new HomeCornerContext())
+            {
+                House houseToUpdate = context.Houses.Find(house.Id);
+                house.Id = house.Id;
+                houseToUpdate.Name = house.Name;
+                houseToUpdate.ReleaseDate = house.ReleaseDate;
+                context.SaveChanges();
+            }
+        }
+
+        public static void Delete(int id)
+        {
+            using (var context = new HomeCornerContext())
+            {
+                House house = context.Houses.Find(id);
+                context.Houses.Remove(house);
+                context.SaveChanges();
+            }
         }
     }
 }
